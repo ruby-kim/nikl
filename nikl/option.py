197d2dc41a182fa9854745f7db4f83f2891b1636
kimkyeongnam <kkyy0126@naver.com>
2020-04-16 13:53:36 +0900

    [Modify] get file name array

--- nikl/option.py
+++ nikl/option.py
@@ -16,7 +16,8 @@ def setting_parse():
             :param: content(boolean): select to get the content of .txt file, <text></text>
     """
     parser = argparse.ArgumentParser(description='옵션을 선택해주세요.', formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--filename', required=True, help='전처리 할 txt 파일명을 입력해주세요: text1.txt \n\n')
+    parser.add_argument('--filenames', required=True, nargs='+', help='전처리 할 txt 파일명을 입력해주세요: text1.txt text2.txt text3.txt\n'
+                                                                      '한 개 이상의 값을 입력해주세요.\n\n')
     parser.add_argument('--info', required=False, action='store_true', help='해당 txt 파일의 정보를 추출할건지 알려주세요: [True / False]\n'
                                                                             '기본값은 False 입니다.\n\n')
     parser.add_argument('--content', required=False, action='store_true', help='해당 txt 파일의 내용을 추출할건지 알려주세요: [True / False]\n'
@@ -26,5 +27,5 @@ def setting_parse():
                              '기본값은 True 입니다.\n\n')
 
     args = parser.parse_args()
-    filename, info, content, newline = args.filename, args.info, args.content, args.newline
-    return filename, info, content, newline
+    filenames, info, content, newline = args.filenames, args.info, args.content, args.newline
+    return filenames, info, content, newline
